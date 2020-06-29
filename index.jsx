9a087d7e7ea5cfaae36d52e18521dc30bb29b994
xuetengfei <18392015462@163.com>
2020-06-29 23:33:54 +0800

    refactor: dist

--- index.jsx
+++ index.jsx
@@ -1,41 +1,42 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import loadable from '@loadable/component';
 import 'regenerator-runtime/runtime';
 import 'intersection-observer';
 import 'spectre.css';
 
-const Home = loadable(() => import('./page/Home/index'));
-const LAZYLOAD = loadable(() => import('./page/lazyLoad/index.jsx'));
-const KeepAlive = loadable(() => import('./page/KeepAlive/index.jsx'));
-const LazyLoadImage = loadable(() => import('./page/LazyLoadImage/index.tsx'));
-const ConcurrentRestrictor = loadable(() => import('./page/ConcurrentRestrictor'));
-const NoMatch = loadable(() => import('./page/NoMatch/index.jsx'));
+const Home = lazy(() => import('./page/Home/index'));
+const LAZYLOAD = lazy(() => import('./page/lazyLoad/index.jsx'));
+const KeepAlive = lazy(() => import('./page/KeepAlive/index.jsx'));
+const LazyLoadImage = lazy(() => import('./page/LazyLoadImage/index.tsx'));
+const ConcurrentRestrictor = lazy(() => import('./page/ConcurrentRestrictor'));
+const NoMatch = lazy(() => import('./page/NoMatch/index.jsx'));
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/lazyload">
-        <LAZYLOAD />
-      </Route>
-      <Route exact path="/keepalive">
-        <KeepAlive />
-      </Route>
-      <Route exact path="/lazyLoadImage">
-        <LazyLoadImage />
-      </Route>
-      <Route exact path="/concurrentrestrictor">
-        <ConcurrentRestrictor />
-      </Route>
-      <Route>
-        <NoMatch />
-      </Route>
-    </Switch>
-  </Router>
+  <Suspense fallback={<div className="loading loading-lg"></div>}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/lazyload">
+          <LAZYLOAD />
+        </Route>
+        <Route exact path="/keepalive">
+          <KeepAlive />
+        </Route>
+        <Route exact path="/lazyLoadImage">
+          <LazyLoadImage />
+        </Route>
+        <Route exact path="/concurrentrestrictor">
+          <ConcurrentRestrictor />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
+  </Suspense>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
