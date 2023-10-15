c36fb51508e241aa20c3dc6ff3e7c9d1ed234961
xuetengfei <workingxue@gmail.com>
2023-10-15 18:07:46 +0800

    2023-10-15 18:07

--- page/Home/index.jsx
+++ page/Home/index.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import routerList from '../../routers';
-import 'spectre.css';
 import './style.less';
 
 export default function index() {
   let history = useHistory();
   return (
     <>
-      <div className="container ">
+      <div className="container">
         <div className="columns">
           {routerList
             .filter(v => v.title)
             .map(({ title, path }, id) => (
               <div
                 key={id}
-                className=" borderStyle column col-xm-4 col-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 p0"
+                className="borderStyle column col-xm-4 col-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 p0"
                 onClick={() => history.push(path)}
               >
                 {id + 1}: {title}
